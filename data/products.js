5126376496e3cb27f508be8bcf1393e2d6d3bebf
rufusreddy <rufusreddy26@gmai.com>
2024-09-02 23:09:15 +0530

    load products from the backend

--- data/products.js
+++ data/products.js
@@ -56,6 +56,28 @@ class Clothing extends Product {
   }
 }
 
+export let products = [];
+
+export function loadProducts(fun) {
+  const xhr = new XMLHttpRequest();
+
+  xhr.addEventListener("load", () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if (productDetails.type === "Clothing") {
+        return new Clothing(productDetails);
+      }
+      return new Product(productDetails);
+    });
+    console.log("load products");
+
+    fun();
+  });
+
+  xhr.open("GET", "https://supersimplebackend.dev/products");
+  xhr.send();
+}
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -533,3 +555,4 @@ export const products = [
   }
   return new Product(productDetails);
 });
+*/
