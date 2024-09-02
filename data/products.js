17daa60b4deb7acb80b4f77155a89dff6e0205ce
rufusreddy <rufusreddy26@gmai.com>
2024-09-02 12:23:35 +0530

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,10 +1,39 @@
+import { formatCurrency } from "../scripts/utils/money.js";
+
 export function getProduct(productId) {
-  let matchingProduct = products.find((product) => product.id === productId);
-  /* if (!matchingProduct) {
-    console.error(`Product with ID ${productId} not found.`);
-    return; */
+  let matchingProduct;
+
+  products.forEach((product) => {
+    if (product.id === productId) {
+      matchingProduct = product;
+    }
+  });
 
   return matchingProduct;
+}
+
+class product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails) {
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl() {
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice() {
+    return ` $${formatCurrency(this.priceCents)}`;
+  }
 }
 
 export const products = [
@@ -478,4 +507,6 @@ export const products = [
     priceCents: 2400,
     keywords: ["sweaters", "hoodies", "apparel", "mens"],
   },
-];
+].map((productDetails) => {
+  return new product(productDetails);
+});
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -39,9 +39,7 @@ export function renderOrderSummary() {
           <img class="product-image" src="${matchingProduct.image}">
           <div class="cart-item-details">
             <div class="product-name">${matchingProduct.name}</div>
-            <div class="product-price">$${formatCurrency(
-              matchingProduct.priceCents
-            )}</div>
+            <div class="product-price">${matchingProduct.getPrice()}</div>
             <div class="product-quantity">
               <span> Quantity: <span class="quantity-label">${
                 cartItem.quantity
