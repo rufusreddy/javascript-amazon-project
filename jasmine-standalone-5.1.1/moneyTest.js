e2c8eda41663570edd30f7a0d033137d6792db18
rufusreddy <rufusreddy26@gmai.com>
2024-09-03 00:11:31 +0530

    learn promises and convert code to promises

--- jasmine-standalone-5.1.1/moneyTest.js
+++ jasmine-standalone-5.1.1/moneyTest.js
@@ -1,4 +1,5 @@
 import { formatCurrency } from "../scripts/utils/money.js";
+import { loadProducts } from "../data/products.js";
 
 describe("test suite: formatCurrency", () => {
   it("converts cents into dollars", () => {
@@ -10,5 +11,11 @@ describe("test suite: formatCurrency", () => {
   });
   it("round up to the nearest cent ", () => {
     expect(formatCurrency(2000.5)).toEqual("20.01");
+  });
+});
+
+beforeAll((done) => {
+  loadProducts(() => {
+    done();
   });
 });
